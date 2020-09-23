8824643b80aaf59e75fcb6d04eade89338303894
lesor9 <lesor@protonmail.coml>
2020-09-23 16:26:57 +0300

    implemented task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+  let nStr = String(n);
+  let finalStr = '';
+
+  for (let i = nStr.length - 1; i >= 0; i--) {
+  	finalStr += nStr[i];
+  }
   
+  if (finalStr[finalStr.length - 1] == '-') finalStr = finalStr.slice(0, -1);
+
+  return Number(finalStr);
 }
